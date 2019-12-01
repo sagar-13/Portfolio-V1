c0c835de2223f0927925573ccd96e13205d04b53
sagar-13 <sagar_13@live.com>
2019-11-30 23:48:33 -0500

    Revamped home page particle overlay, made particle text visible on mobile, overlayed content on top of canvas properly.

--- js/effects.js
+++ js/effects.js
@@ -28,7 +28,7 @@
             x : x,
             y: y
         };
-        this.r =  Math.random()*5 + 2;
+        this.r =  Math.random()*1 + 2;
         this.vx = (Math.random()-0.5)*20;
         this.vy = (Math.random()-0.5)*20;
         this.accX = 0;
@@ -137,145 +137,15 @@ function onTouchEnd(){
     requestAnimationFrame(render);
 	
 	
-	
-	
-//var canvas = document.querySelector("#scene"),  
-//  ctx = canvas.getContext("2d"),
-//        particles = [],
-//        amount = 0,
-//        mouse = {x:0,y:0},
-//        radius = 1;
-//
-//    var colors = ["#468966","#FFF0A5", "#FFB03B","#B64926", "#8E2800"];
-//
-//
-//
-//    var ww = canvas.width = window.innerWidth;
-//    var wh = canvas.height = window.innerHeight/2;
-//
-//    function Particle(x,y){
-//        this.x =  Math.random()*ww;
-//        this.y =  Math.random()*wh;
-//        this.dest = {
-//            x : x,
-//            y: y
-//        };
-//        this.r =  Math.random()*2 + 3;
-//        this.vx = (Math.random()-0.5)*20;
-//        this.vy = (Math.random()-0.5)*10;
-//        this.accX = 0;
-//        this.accY = 0;
-//        this.friction = Math.random()*0.001 + 0.94;
-//
-//        this.color = colors[Math.floor(Math.random()*10)];
-//    }
-//
-//    Particle.prototype.render = function() {
-//
-//
-//        this.accX = (this.dest.x - this.x)/600;
-//        this.accY = (this.dest.y - this.y)/600;
-//        this.vx += this.accX;
-//        this.vy += this.accY;
-//        this.vx *= this.friction;
-//        this.vy *= this.friction;
-//
-//        this.x += this.vx;
-//        this.y +=  this.vy;
-//
-//        ctx.fillStyle = this.color;
-//        ctx.beginPath();
-//        ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
-//        ctx.fill();
-//
-//        var a = this.x - mouse.x;
-//        var b = this.y - mouse.y;
-//
-//        var distance = Math.sqrt( a*a + b*b );
-//        if(distance<(radius*70)){
-//            this.accX = (this.x - mouse.x)/100;
-//            this.accY = (this.y - mouse.y)/100;
-//            this.vx += this.accX;
-//            this.vy += this.accY;
-//        }
-//
-//    }
-//
-//    function onMouseMove(e){
-//        mouse.x = e.clientX;
-//        mouse.y = e.clientY;
-//    }
-//
-//    function onTouchMove(e){
-//    if(e.touches.length > 0 ){
-//      mouse.x = e.touches[0].clientX;
-//      mouse.y = e.touches[0].clientY;
-//    }
-//    }
-//
-//function onTouchEnd(){
-//  mouse.x = -9999;
-//  mouse.y = -9999;
-//}
-//
-//    function initScene(){
-//        ww = canvas.width = window.innerWidth;
-//        wh = canvas.height = window.innerHeight/2;
-//
-//        ctx.clearRect(0, 0, canvas.width, canvas.height);
-//
-//        ctx.font = "bold "+(ww/10)+"px sans-serif";
-//        ctx.textAlign = "center";
-//        ctx.fillText("Hire Me!", ww/2, wh/2);
-//
-//        var data  = ctx.getImageData(0, 0, ww, wh).data;
-//        ctx.clearRect(0, 0, canvas.width, canvas.height);
-//        ctx.globalCompositeOperation = "screen";
-//
-//        particles = [];
-//        for(var i=0;i<ww;i+=Math.round(ww/150)){
-//            for(var j=0;j<wh;j+=Math.round(ww/150)){
-//                if(data[ ((i + j*ww)*4) + 3] > 150){
-//                    particles.push(new Particle(i,j));
-//                }
-//            }
-//        }
-//        amount = particles.length;
-//
-//    }
-//	
-//	
-//
-//
-//
-//
-//    function onMouseClick(){
-//        radius++;
-//        if(radius ===5){
-//            radius = 0;
-//        }
-//    }
-//
-//    function render() {
-//        requestAnimationFrame(render);
-//        ctx.clearRect(0, 0, canvas.width, canvas.height);
-//        for (var i = 0; i < amount; i++) {
-//            particles[i].render();
-//        }
-//    }
-//
-//
-//    window.addEventListener("resize", initScene);
-//    window.addEventListener("mousemove", onMouseMove);
-//    window.addEventListener("touchmove", onTouchMove);
-//    window.addEventListener("click", onMouseClick);
-//    window.addEventListener("touchend", onTouchEnd);
-//
-//    requestAnimationFrame(render);
-//
-//
-//
-
+	$(window).scroll(function() {
+		var hT = $('#scene').offset().top,
+			hH = $('#scene').outerHeight(),
+			wH = $(window).height(),
+			wS = $(this).scrollTop();
+		if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+			initScene();
+		}
+	});
 
 
 
